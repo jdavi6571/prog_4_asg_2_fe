1fe38d8f4a26c5476a7d1e1e0a75639f1692d309
Doug C <d.chung1245@gmail.com>
2018-11-09 00:53:25 -0700

    Chart works

--- screens/MoreDetails.js
+++ screens/MoreDetails.js
@@ -1,49 +1,78 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Dimensions } from 'react-native';
 import { Card, ListItem, Button } from 'react-native-elements';
-import { LineChart, Dimensions } from 'react-native-chart-kit';
+import { LineChart } from 'react-native-chart-kit';
 
 //export default (props) => <Text>hmm {JSON.stringify(props, null, 2)}</Text>
 
 export default class MoreDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {initialized: false};
+  }
+  onLayout = (e) => {
+    this.setState({
+      initialized: true,
+      width: e.nativeEvent.layout.width,
+      height: e.nativeEvent.layout.height,
+      x: e.nativeEvent.layout.x,
+      y: e.nativeEvent.layout.y
+    })
+  }
   render() {
+
+    const { itemData } = this.props.navigation.state.params;
+    const currentPrice = itemData.currentDollarValue;
+    const parsed = Number(currentPrice.replace(/[^0-9.-]+/g,""));
+
+    const fakeValues = [
+      Math.random() * 5,
+      Math.random() * 5,
+      Math.random() * 5,
+      Math.random() * 5,
+      Math.random() * 5,
+      Math.random() * 5
+    ].map(randomValue =>
+      parsed + randomValue
+    );
+
+    fakeValues.push(parsed);
+
+
     return (
       <Card>
+        <Text>{currentPrice} : {parsed}</Text>
         <Text>
-          {JSON.stringify(this.props.navigation.state.params, null, 2)}
-          <LineChart
-            data={{
-              labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-              datasets: [{
-                data: [
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100,
-                  Math.random() * 100
-                ]
-              }]
-            }}
-            width={Dimensions.get('window').width} // from react-native
-            height={220}
-            chartConfig={{
-              backgroundColor: '#e26a00',
-              backgroundGradientFrom: '#fb8c00',
-              backgroundGradientTo: '#ffa726',
-              decimalPlaces: 2, // optional, defaults to 2dp
-              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-              style: {
-                borderRadius: 16
-              }
-            }}
-            bezier
-            style={{
-              marginVertical: 8,
-              borderRadius: 16
-            }}
-          />
+          {JSON.stringify(this.props.navigation.state.params.itemData, null, 2)}
         </Text>
+        <View onLayout={this.onLayout}>
+          {this.state.initialized &&
+            <LineChart
+              data={{
+                labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June'],
+                datasets: [{
+                  data: fakeValues
+                }]
+              }}
+              width={this.state.width} // from react-native
+              height={220}
+              chartConfig={{
+                backgroundColor: '#e26a00',
+                backgroundGradientFrom: '#fb8c00',
+                backgroundGradientTo: '#ffa726',
+                decimalPlaces: 2, // optional, defaults to 2dp
+                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                style: {
+                  borderRadius: 16
+                }
+              }}
+              style={{
+                marginVertical: 8,
+                borderRadius: 16
+              }}
+            />
+          }
+        </View>
       </Card>
     )
   }
