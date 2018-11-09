fbeaa9d29236783575c3a9a460ab1de4e84a5f4a
Doug C <d.chung1245@gmail.com>
2018-11-09 00:22:00 -0700

    Started on chart in MoreDetails.js

--- screens/MoreDetails.js
+++ screens/MoreDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native';
 import { Card, ListItem, Button } from 'react-native-elements';
+import { LineChart, Dimensions } from 'react-native-chart-kit';
 
 //export default (props) => <Text>hmm {JSON.stringify(props, null, 2)}</Text>
 
@@ -10,6 +11,38 @@ export default class MoreDetails extends React.Component {
       <Card>
         <Text>
           {JSON.stringify(this.props.navigation.state.params, null, 2)}
+          <LineChart
+            data={{
+              labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+              datasets: [{
+                data: [
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100
+                ]
+              }]
+            }}
+            width={Dimensions.get('window').width} // from react-native
+            height={220}
+            chartConfig={{
+              backgroundColor: '#e26a00',
+              backgroundGradientFrom: '#fb8c00',
+              backgroundGradientTo: '#ffa726',
+              decimalPlaces: 2, // optional, defaults to 2dp
+              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              style: {
+                borderRadius: 16
+              }
+            }}
+            bezier
+            style={{
+              marginVertical: 8,
+              borderRadius: 16
+            }}
+          />
         </Text>
       </Card>
     )
