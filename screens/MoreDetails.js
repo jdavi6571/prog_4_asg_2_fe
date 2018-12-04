4926e9a93acb58416d6ee15923fa9b5f2eead9d0
Joshua Sturgeon <JoshuaSturgeon@Joshuas-MacBook-Pro-2.local>
2018-12-03 20:06:56 -0700

    Completed crypto list call

--- screens/MoreDetails.js
+++ screens/MoreDetails.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { View, Text, Image, Dimensions, ScrollView } from 'react-native';
 import { Card, ListItem, Button } from 'react-native-elements';
 import { LineChart } from 'react-native-chart-kit';
-
-//export default (props) => <Text>hmm {JSON.stringify(props, null, 2)}</Text>
+import Converter from '../procedures/CoinConverter.js';
 
 export default class MoreDetails extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       initialized: false,
+      coinID: '',
       price: 0,
       changePct: 0,
       volume24: 0
@@ -27,13 +27,15 @@ export default class MoreDetails extends React.Component {
   }
 
   getSingleDetails() {
-    var coinID =  this.props.navigation.state.params.itemData.marketName;
-    fetch('https://min-api.cryptocompare.com/data/pricemultifull?fsyms='+coinID+'&tsyms=CDN&api_key=584fd3b43d00f3e846f51724756eed798b5814d35ce60374a210dd2277baeb48')
+    var id = this.props.navigation.state.params.marketKey;
+
+    fetch('https://min-api.cryptocompare.com/data/pricemultifull?fsyms='+id+'&tsyms=CAD&api_key=584fd3b43d00f3e846f51724756eed798b5814d35ce60374a210dd2277baeb48')
       .then((response) => response.json() )
       .then((responseJSON) => {
-        var results = eval('responseJSON.RAW.'+coinID+'.CDN');
+        var results = eval('responseJSON.RAW.'+id+'.CAD');
 
         this.setState({
+          coinID: id,
           price: results.PRICE.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'),
           changePct: results.CHANGEPCT24HOUR.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'),
           volume24: results.TOTALVOLUME24H.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')
@@ -43,25 +45,8 @@ export default class MoreDetails extends React.Component {
   }
 
   render() {
-
-    const { itemData } = this.props.navigation.state.params;
-    const currentPrice = itemData.currentDollarValue;
-    const parsed = Number(currentPrice.replace(/[^0-9.-]+/g,""));
-
-    const fakeValues = [
-      Math.random() * 5,
-      Math.random() * 5,
-      Math.random() * 5,
-      Math.random() * 5,
-      Math.random() * 5,
-      Math.random() * 5
-    ].map(randomValue =>
-      parsed + randomValue
-    ).concat(parsed);
-
-
     return (
-      <Card title={itemData.name}>
+      <Card title={this.state.coinID}>
         <ScrollView>
           <View>
             <View>
@@ -83,36 +68,55 @@ export default class MoreDetails extends React.Component {
               </View>
             </View>
           </View>
-          <View onLayout={this.onLayout}>
-            {this.state.initialized &&
-              <LineChart
-                data={{
-                  labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June'],
-                  datasets: [{
-                    data: fakeValues
-                  }]
-                }}
-                width={this.state.width} // from react-native
-                height={220}
-                chartConfig={{
-                  backgroundColor: '#e26a00',
-                  backgroundGradientFrom: '#fb8c00',
-                  backgroundGradientTo: '#ffa726',
-                  decimalPlaces: 2, // optional, defaults to 2dp
-                  color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                  style: {
-                    borderRadius: 16
-                  }
-                }}
-                style={{
-                  marginVertical: 8,
-                  borderRadius: 16
-                }}
-              />
-            }
-          </View>
+
+          <Converter conversionRate={this.state.price} />
+
           </ScrollView>
       </Card>
     )
   }
 }
+
+/*
+const parsed = Number(this.state.price.replace(/[^0-9.-]+/g,""));
+
+const fakeValues = [
+  Math.random() * 5,
+  Math.random() * 5,
+  Math.random() * 5,
+  Math.random() * 5,
+  Math.random() * 5,
+  Math.random() * 5
+].map(randomValue =>
+  parsed + randomValue
+).concat(parsed);
+
+<View onLayout={this.onLayout}>
+  {this.state.initialized &&
+    <LineChart
+      data={{
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June'],
+        datasets: [{
+          data: fakeValues
+        }]
+      }}
+      width={this.state.width} // from react-native
+      height={220}
+      chartConfig={{
+        backgroundColor: '#e26a00',
+        backgroundGradientFrom: '#fb8c00',
+        backgroundGradientTo: '#ffa726',
+        decimalPlaces: 2, // optional, defaults to 2dp
+        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        style: {
+          borderRadius: 16
+        }
+      }}
+      style={{
+        marginVertical: 8,
+        borderRadius: 16
+      }}
+    />
+  }
+</View>
+*/
